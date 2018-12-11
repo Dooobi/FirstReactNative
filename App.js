cb84cc96750aa004bde65cbfbf0c6a05af348799
Demindh <tobias.stelzer93@gmx.de>
2018-12-11 23:24:39 +0100

    Text styles

--- App.js
+++ App.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import BlinkApp from './components/BlinkApp.js';
+import LotsOfStyles from './components/LotsOfStyles.js';
 
 export default class App extends Component {
   render() {
     return (
       <View style={styles.container}>
-        <BlinkApp/>
+        <LotsOfStyles/>
       </View>
     );
   }
